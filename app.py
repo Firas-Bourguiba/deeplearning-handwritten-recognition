b7eb2c9672e1009adaa03b98b63482dbaf2c6e78
firas bourguiba <fbourguiba18@gmail.com>
2024-05-03 20:03:23 +0100

    update train

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, request, render_template
-import tensorflow as tf
-import numpy as np
-import cv2
+from flask import Flask, request, render_template  #importatiion de flask pour la création d'une application web
+import tensorflow as tf  #importer tensorflow pour la creation du model et l'entrainement
+import numpy as np   #importation de numpy pour le calcul scientifique avec un support pour les tableaux et les matrices
+import cv2    #importation de cv2 pour la manipulation d'images
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('model.h5') # Chargement du model sauvegardé hdf5
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET']) #route pour la page d'accueil
 def index():
     # Render the upload HTML form
     return render_template("index.html")
 
 @app.route('/predict', methods=['POST'])
-def predict_digit():
+def predict_digit():                       #Fonction pour prédire le chiffre d'une image envoyée. Si aucune image n'est envoyée, elle affiche de nouveau le formulaire sans prédiction.
     file = request.files.get('image')
     if not file:
         return render_template("index.html", prediction=None)
 
     # Read the image through OpenCV, convert to grayscale, and resize
-    image = np.array(cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_GRAYSCALE))
-    image = cv2.resize(image, (28, 28))
+    image = np.array(cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_GRAYSCALE)) #Lecture de l'image via OpenCV, conversion en niveaux de gris et décodage de l'image à partir des données brutes envoyées.
+    image = cv2.resize(image, (28, 28)) # redimensionnement de l'image en 28x28
     image = np.invert(image)  # Invert colors
     image = image.reshape(1, 28, 28, 1)  # Reshape for the model
 
--- train.py
+++ train.py
@@ -1,35 +1,34 @@
-import tensorflow as tf
+import tensorflow as tf #importer tensorflow pour la creation du model et l'entrainement
 
-def train_and_save():
-    # Load the MNIST dataset
-    mnist = tf.keras.datasets.mnist
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+def train_and_save(): # def d'une fonction qui encapsule le processus d'entrainement et de sauvegarde du model
+    
+    mnist = tf.keras.datasets.mnist  #importer le dataset mnist
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()  # découpage des doonées MNIST en données d'entrainement ('X_train', 'y_train') et données de test ('X_test', 'y_test')
 
-    # Normalize the data
-    X_train, X_test = X_train / 255.0, X_test / 255.0
+    
+    X_train, X_test = X_train / 255.0, X_test / 255.0 #  # Normalisation des données en divisant par 255.0 pour qu'elles soient comprises entre 0 et 1 pour accélérer l'entrainement
 
     # Build the neural network model
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
+    model = tf.keras.models.Sequential(                    # Création d'un modèle séquentiel pour creer des modeles couche par couche pour la plupart des problèmes
+        tf.keras.layers.Flatten(input_shape=(28, 28)),      # Aplatir les données d'entrée en un vecteur 1D ou transfromation de l'image 28x28 (2d) en un vecteur 1D de 784
+        tf.keras.layers.Dense(128, activation='relu'),      # Ajout d'une couche dense'(entierement connécté) de 128 neurones avec une fonction d'activation 'relu'
+        tf.keras.layers.Dense(128, activation='relu'),      # Ajout d'une autre couche dense de 128 neurones avec une fonction d'activation 'relu' pour introduire de la non-linéarité
+        tf.keras.layers.Dense(10, activation='softmax'))     # Ajout d'une couche de sortie de 10 neurones un pour chaque classe de chiffres (0-9) avec une fonction d'activation 'softmax' pour la classification multiclasse
 
-    # Compile the model
+    # Compilation du model en spécifiant l'optimiseur adam, la fonction de perte 'sparse_categorical_crossentropy' et la métrique 'accuracy' (précision)
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    # Train the model
+    # entraiement du model sur les données d'entrainement 'X_train', 'y_train' avec 3 époques et validation sur les données de test 'X_test', 'y_test' pour chaque époque
     model.fit(X_train, y_train, epochs=3, validation_data=(X_test, y_test))
 
-    # Save the model to disk
+    # Sauvegarde du model en format hdf5
     model.save('model.h5')
 
-    # Evaluate the model
+    # Evaluation du model sur les données de test 'X_test', 'y_test' et affichage de la perte et de la précision
     val_loss, val_acc = model.evaluate(X_test, y_test)
-    print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}")
+    print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}") # affichage de la perte et de la précision
 
-if __name__ == '__main__':
+if __name__ == '__main__': # si le script est exécuté directement, appeler la fonction 'train_and_save'
     train_and_save()
